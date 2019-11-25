50f1d235785429bd0795a75dbd72318197787f66
brsh <brsh@users.noreply.github.com>
2019-11-25 09:27:38 -0800

    Silenced initial help display on autoload

--- brshLinuxAnalogs.psm1
+++ brshLinuxAnalogs.psm1
@@ -4,6 +4,7 @@
 #region Default Private Variables
 # Current script path
 [string] $script:ScriptPath = Split-Path (Get-Variable MyInvocation -scope script).value.MyCommand.Definition -Parent
+if ((Get-Variable MyInvocation -Scope script).Value.Line.Trim().Length -eq 0) { $Quiet = $true }
 [string[]] $script:ShowHelp = @()
 # if ($PSVersionTable.PSVersion.Major -lt 6) {
 # 	[bool] $IsLinux = $false
